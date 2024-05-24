5bf270f3eb84d672753ac8b2c9269db77d15a6dc
Coding-byKS <khushboo.singh43@gmail.com>
2024-05-24 11:15:18 +0100

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import md5 from "md5";
 import MyTopTen from "./MyTopTen";
 import "./App.css";
+import UserShowSearch from "./UserShowSearch";
 
 
 export default function App() {
@@ -9,72 +10,45 @@ export default function App() {
   
   const [showInput, getShow]=useState("");
   const [userShow, fetchShow]=useState("");
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const now = Date.now();
-      const publicKey = "5a80fc9cd71c4feac54e5ba0f521debc";
-      const privateKey = "cc06371e7c3fac6c31860f0644283ed1d48f4b13";
-      let params = {
-        apikey: publicKey,
-        ts: now
-      };
-      params.hash = md5(params.ts + privateKey + publicKey);
-
-      const apiUrl = "https://gateway.marvel.com/v1/public/characters";
-
-      const results = await fetch(
-        `${apiUrl}?apikey=${params.apikey}&ts=${params.ts}&hash=${params.hash}`
-      )
-        // .then(res => res.json())
-        // .then(res => res.data)
-        // .then(res => res.results);
-        let marv = await results.json();
-        // console.log(marv.data.results);
-      setData(marv.data.results);
-    };
-
-    fetchData();
-  }, []);
+ 
 
   function handleSubmit(event){
+    
     event.preventDefault();
     console.log(showInput);
     fetchShow(showInput);
-
-
+    getShow("");
   }
+
   function handleChange(e){
     
     getShow(e.target.value)
     console.log(showInput)
 
   }
+
+  function handleReset(){
+    fetchShow("");
+  }
+
+  
   return (
     <div className="App">
       <h1>Hi, which TV show would like to know more about?</h1>
-      <form>
-        <input required type="text" onChange={handleChange}></input>
-        <input onClick={handleSubmit} type="submit" />
+      <form style={{fontSize:"70px"}}>
+        {!userShow&&<input required type="text" onChange={handleChange} value={showInput}></input>}
+        {!userShow&&<input onClick={handleSubmit} type="submit" />}
+        {userShow&&<button onClick={handleReset}>Search Another Show</button>}
       </form>
-      {userShow&&<MyTopTen show={userShow}/>}
+      
+      {userShow&&<UserShowSearch show={userShow}/>}
       <h2>Here are some of my favorites:</h2>
 
       <div className="container">
         
       {shows.map((show) => <div className="show"><MyTopTen show={show}/></div>)}
       </div>
-      {/* <h1>Marvel API</h1>
-
-      {data.map((char, index) => (
-        <div key={char.id.toString()}>
-          <p>
-            {index} - {char.name}
-            <img src={char.thumbnail.path+"."+char.thumbnail.extension}/>
-          </p>
-          {index < data.length - 1 ? <hr /> : ""}
-        </div>
-      ))} */}
+      
     </div>
   );
 }
--- src/UserShowSearch.jsx
+++ src/UserShowSearch.jsx
@@ -2,22 +2,24 @@ import { useState, useEffect } from 'react';
 // import {v4 as uuid} from "uuid";
 
 import "./MyTopTen.css";
-function MyTopTen(props) {
+function UserShowSearch(props) {
   
   const [show, setShow] = useState([]);
   const [summary, setSummary] = useState("");
   const [image, setImage] = useState("");
   const [rating, setRating] = useState("");
-  let ifValid=false;
+  const [ifValid, setValid] =useState(false);
   
   const fetchHandler = async() => {
     try{
     const response = await fetch(`https://api.tvmaze.com/singlesearch/shows?q=${props.show}`);
     if(!response.ok){
       throw new Error(response.statusText);
+     
     }
     const data = await response.json();  
     // console.log(data);
+    setValid(true);
     setShow(data);
     setImage(data.image.original);
     setRating(data.rating.average);
@@ -35,36 +37,19 @@ fetchHandler();
   return (
 
     
-    <div className='showContainer'>
-       
+    ifValid?(<div><h2>Here you go!</h2>  <div className='showContainer'>
+        
+        
      <div style={{fontSize:"40px", fontWeight:"bolder"}}>{show.name}</div>
      <img src={image} style={{width:"400px", maxHeight:"500px"}}/>     
      <div style={{fontSize:"20px"}}>Language: {show.language}</div>      
      <div style={{fontSize:"20px"}}>Average Rating: {rating}</div>
      <a style={{fontSize:"20px", color:"black"}} href={show.officialSite}>Official Site: {show.officialSite}</a>
-     <div style={{fontSize:"20px"}}>{summary}</div>
-      {/* <button onClick={fetchHandler}>REFRESH</button> */}      
-    </div>
+     <div style={{fontSize:"20px"}}>{summary}</div>    
+    </div> </div>):(<h2>Sorry, could not find it. Enter a valid TV Show Name.</h2>)
+    
+    // <h2>Enter a valid TV Show.</h2>
 )
 }
 
-export default MyTopTen;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default UserShowSearch;
